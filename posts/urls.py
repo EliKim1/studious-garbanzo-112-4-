eeca3ed70b2b3c9f7aa16c8ee1ac49ba87123279
EliKim1 <elikimq@gmail.com>
2022-09-28 23:45:30 -0500

    heroku

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, HomePage, AboutPage,
+from .views import (PostListView,
+PostListView,
+DraftPostListView,
+PostDetailView,
+PostCreateView,
+PostUpdateView,
+PostDeleteView,
+)
 
 
   
 urlpatterns = [
-    path('list/', PostListView.as_view(), name='list'),
+    path('', PostListView.as_view(), name='list'),
+    path('drafts/', DraftPostListView.as_view(), name='draft_list'),
     path('<int:pk>/', PostDetailView.as_view(), name='detail'),
     path('new/', PostCreateView.as_view(), name='new'),
-    path('<int:pk>/update', PostUpdateView.as_view(), name='update'),
+    path('<int:pk>/edit', PostUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete', PostDeleteView.as_view(), name='delete'),
-    path('', HomePage.as_view(), name='blog'),
-    path('', AboutPage.as_view(), name='about'),
+  
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,25 +2,41 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.url import reverse_lazy
-from .models import Post
+from .models import Post, Status
 
 # Create your views here.
 # path('', HomePage.as_view(), name='blog')
-class PostHomePageView(HomePageView):
-    templates_name = "posts/home.html"
-
-class PostListPage(TemplateView):
-    template_name = "posts/home.html"
+class PostListView(HomePageView):
+    templates_name = "posts/list.html"
     model = Post
+
+class DraftPostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pending_status = Status.objects.get(name='draft')
+        context['post_list'] = Post.objects.filter(
+            author=self.request.user
+            ).filter(
+                status=pending_status).order_by(
+                'created_on').reverse()
+        
+        return context
 
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/home.html"
     model = Post
     fields = ["title", "subtitle", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostListView(ListView):
     template_name = "posts/list.html"
